e77fdedcf90faea4485c1bf7ea4486babb9fae81
Kirill <kirill.rasshivalov@gmail.com>
2025-11-01 13:05:12 +0300

    Добавил сервис для основных опираций над лаб ворками.

---
+++ main/java/algo/demo/services/LabWorkService.java
@@ -0,0 +1,65 @@
+package algo.demo.services;
+
+import algo.demo.database.*;
+import algo.demo.dto.LabWork;
+import algo.demo.repository.LabWorkRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@ApplicationScoped
+public class LabWorkService {
+
+    private static final Logger logger = LoggerFactory.getLogger(LabWorkService.class.getName());
+
+    @Inject
+    private LabWorkRepository labWorkRepository;
+
+    public LabWorkTable addLabWork(LabWork labWork) {
+        try {
+            return labWorkRepository.addLbWork(labWork);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public LabWorkTable updateLabWork(Long id, LabWork labWork) {
+        try {
+            return labWorkRepository.updateLabWorkById(id, labWork);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public void deleteLabWork(Long id) {
+        try {
+            labWorkRepository.deleteLabWorkById(id);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public List<LabWorkTable> getAllLabWorks() {
+        try {
+            return labWorkRepository.getAllLabWorkTables();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public LabWorkTable getLabWorkById(Long id) {
+        try {
+            return labWorkRepository.getLabWorkById(id);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+}
