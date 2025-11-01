4b1e6878e68d9173bbc22e55554c6147aed43d29
Kirill <kirill.rasshivalov@gmail.com>
2025-11-01 12:53:41 +0300

    Добавил тестовый контроллер через который можно стучаться на сервер для проверки жив ли он.

---
+++ main/java/algo/demo/controllers/PingController.java
@@ -0,0 +1,18 @@
+package algo.demo.controllers;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/test")
+public class PingController {
+
+    @GET
+    @Path("/ping")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response ping() {
+        return Response.ok("PONG!").build();
+    }
+}
