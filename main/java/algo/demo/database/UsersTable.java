0630a92ebb4fa170baa6426d2f95e3f85f72f0f6
Kirill <kirill.rasshivalov@gmail.com>
2025-11-04 22:07:28 +0300

    Добавил таблицу с юзерами.

---
+++ main/java/algo/demo/database/UsersTable.java
@@ -0,0 +1,23 @@
+package algo.demo.database;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="users")
+public class UsersTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="username")
+    private String name;
+
+    @Column(name="password")
+    private String password;
+
+}
