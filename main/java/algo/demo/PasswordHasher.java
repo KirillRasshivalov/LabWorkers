014d3a2dcd0455ca09f0ca6cece93767f0086548
Kirill <kirill.rasshivalov@gmail.com>
2025-11-04 22:08:33 +0300

    Написал хэшер паролей для пользователей.

--- main/java/algo/demo/PasswordHasher.java
+++ main/java/algo/demo/PasswordHasher.java
@@ -3,17 +3,18 @@ package algo.demo;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHasher {
-    private final int logRounds;
 
-    public PasswordHasher() {
-        this.logRounds = 12;
-    }
-
-    public PasswordHasher(int logRounds) {
-        this.logRounds = logRounds;
-    }
+    private final int logRounds = 12;
 
     public String hash(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(logRounds));
+    }
+
+    public boolean verify(String password, String hash) {
+        try {
+            return BCrypt.checkpw(password, hash);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
