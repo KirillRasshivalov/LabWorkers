fe9ddb32dcdf015aa58166170ac61006e6a81770
Kirill <kirill.rasshivalov@gmail.com>
2025-11-05 15:12:59 +0300

    Добавил новые методы в сервис авторизации.

--- main/java/algo/demo/services/AuthorisationService.java
+++ main/java/algo/demo/services/AuthorisationService.java
@@ -2,7 +2,10 @@ package algo.demo.services;
 
 import algo.demo.PasswordHasher;
 import algo.demo.dto.UserLoginRequest;
+import algo.demo.exceptions.LoginException;
+import algo.demo.exceptions.RegistrationException;
 import algo.demo.repository.UserRepository;
+import algo.demo.security.JwtUtil;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.slf4j.Logger;
@@ -19,6 +22,9 @@ public class AuthorisationService {
     @Inject
     private UserRepository userRepository;
 
+    @Inject
+    private JwtUtil jwtUtil;
+
     public Boolean isNameInUsers(String username) {
         return userRepository.isNameInUse(username);
     }
@@ -29,16 +35,31 @@ public class AuthorisationService {
     }
 
     public Boolean isUserCorrect(UserLoginRequest userLoginRequest) {
-        return userRepository.isUserExist(userLoginRequest.username(), userLoginRequest.password());
+        if (userRepository.isUserExist(userLoginRequest.username(), userLoginRequest.password())) {
+            return true;
+        }
+        throw new LoginException("Неверный пароль или имя пользователя.");
     }
 
     public void registerUser(UserLoginRequest userLoginRequest) {
-        if (!isNameInUsers(userLoginRequest.username())) {
+        if (!isNameInUsers(userLoginRequest.username()) && isDataCorrect(userLoginRequest)) {
             try {
                 userRepository.registerUser(userLoginRequest.username(), passwordHasher.hash(userLoginRequest.password()));
             } catch (Exception e) {
                 logger.error("Что то пошло не так." + e.getMessage());
+                throw new RegistrationException("Что то пошло не так." + e.getMessage());
             }
-        } else logger.error("Что то пошло не так во время регистрации. Попробуйте другой логин.");
+        } else {
+            logger.error("Что то пошло не так во время регистрации. Попробуйте другой логин.");
+            throw new RegistrationException("Что то пошло не так во время регистрации. Попробуйте другой логин.");
+        }
     }
+
+    public String getToken(UserLoginRequest userLoginRequest) {
+        if (isDataCorrect(userLoginRequest)) {
+            return jwtUtil.generateToken(userLoginRequest.username());
+        }
+        throw new LoginException("Неверные данные для входа. Проверьте пароль и имя пользовтаеля.");
+    }
+
 }
