f8f01e43eb9a630cf611540f5aecd425cab6c5e5
Kirill <kirill.rasshivalov@gmail.com>
2025-11-05 14:38:40 +0300

    Добавил в параметры контроллер специальных операция jwt токен, а также переписал логи.

--- main/java/algo/demo/controllers/SpecialOperationController.java
+++ main/java/algo/demo/controllers/SpecialOperationController.java
@@ -2,9 +2,11 @@ package algo.demo.controllers;
 
 import algo.demo.database.CoordinatesTable;
 import algo.demo.database.LabWorkTable;
+import algo.demo.security.JwtUtil;
 import algo.demo.services.SpecialOperationsService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
@@ -20,11 +22,15 @@ public class SpecialOperationController {
     @Inject
     private SpecialOperationsService specialOperationsService;
 
+    @Inject
+    private JwtUtil jwtUtil;
+
     @GET
     @Path("/middleValue")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response calculateMiddleValueOfMinPoint() {
-        logger.info("Пришел запрос на получение среднего значения поля минимальной оценки.");
+    public Response calculateMiddleValueOfMinPoint(@HeaderParam(HttpHeaders.AUTHORIZATION) String jwtToken) {
+        logger.info("Пришел запрос на получение среднего значения поля минимальной оценки. От пользователя " +
+                jwtUtil.extractUsername(jwtToken));
         try {
             Double middleValue = specialOperationsService.middleValueOfMinPoint();
             logger.info("Успешно найдено медианное знаыение.");
@@ -42,8 +48,9 @@ public class SpecialOperationController {
     @GET
     @Path("/minCoordinates")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response calculateMinCoordinates() {
-        logger.info("Пришел запрос на получение коллекции с минимальными координатами.");
+    public Response calculateMinCoordinates(@HeaderParam(HttpHeaders.AUTHORIZATION) String jwtToken) {
+        logger.info("Пришел запрос на получение коллекции с минимальными координатами. От пользователя " +
+                jwtUtil.extractUsername(jwtToken));
         try {
             LabWorkTable labWorkWithMinCoordinates = specialOperationsService.getLabWorkByMinId();
             logger.info("Успешно найдена коллекция с минимальной координатой.");
@@ -61,8 +68,9 @@ public class SpecialOperationController {
     @GET
     @Path("/minCoordinatesXY")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response calculateMinCoordinatesXY() {
-        logger.info("Пришел запрос на показ минимальных координат X и Y");
+    public Response calculateMinCoordinatesXY(@HeaderParam(HttpHeaders.AUTHORIZATION) String jwtToken) {
+        logger.info("Пришел запрос на показ минимальных координат X и Y. От пользовтаеля: "
+                + jwtUtil.extractUsername(jwtToken));
         try {
             CoordinatesTable coordinatesTableMin = specialOperationsService.getMinCoordinates();
             logger.info("Успешно получена минимальная координата.");
@@ -80,8 +88,9 @@ public class SpecialOperationController {
     @GET
     @Path("/uniqueMass")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response calculateUniqueMass() {
-        logger.info("Пришел запрос на получения уникальных минимальных баллов.");
+    public Response calculateUniqueMass(@HeaderParam(HttpHeaders.AUTHORIZATION) String jwtToken) {
+        logger.info("Пришел запрос на получения уникальных минимальных баллов. От пользователя: " +
+                jwtUtil.extractUsername(jwtToken));
         try {
             List<Integer> unicMinimalPoints = specialOperationsService.getUnicMass();
             logger.info("Успешно получен массив уникальных данных.");
@@ -100,8 +109,12 @@ public class SpecialOperationController {
     @Path("/decrease/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response decreaseLabDifficulty(@PathParam("id") Long id, @QueryParam("steps") Integer steps) {
-        logger.info("Пришел запрос на понижение сложности на " + steps);
+    public Response decreaseLabDifficulty(@HeaderParam(HttpHeaders.AUTHORIZATION) String jwtToken,
+                                          @PathParam("id") Long id,
+                                          @QueryParam("steps") Integer steps)
+    {
+        logger.info("Пришел запрос на понижение сложности на " + steps + ". От пользователя: " +
+                jwtUtil.extractUsername(jwtToken));
         try {
             specialOperationsService.decreaseDifficultInLabWork(id, steps);
             logger.info("Уровень успешно понижен.");
