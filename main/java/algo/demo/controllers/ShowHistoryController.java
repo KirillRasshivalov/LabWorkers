4d9f43499eb1bfb896be9dff59318c712cacc5be
Kirill <kirill.rasshivalov@gmail.com>
2025-11-06 23:58:41 +0300

    Добавил контроллер для показа истории импортов.

---
+++ main/java/algo/demo/controllers/ShowHistoryController.java
@@ -0,0 +1,48 @@
+package algo.demo.controllers;
+
+import algo.demo.database.ImportHistoryTable;
+import algo.demo.services.HistoryService;
+import algo.demo.services.UserService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@Path("/history")
+@RolesAllowed({"ADMIN", "USER"})
+public class ShowHistoryController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ShowHistoryController.class);
+
+    @Inject
+    private UserService userService;
+
+    @Inject
+    private HistoryService historyService;
+
+    @GET
+    @Path("/files_dump")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response filesDumpHistory(@HeaderParam(HttpHeaders.AUTHORIZATION) String jwtToken) {
+        logger.info("Пришел запрос на показ всех файлов которые были импортированы в программу." +
+                " От пользовтаеля: " + userService.getUsername(jwtToken));
+        try {
+            List<ImportHistoryTable> listOfDumps = historyService.getHistoryOfImports(jwtToken);
+            return Response.ok()
+                    .entity(listOfDumps)
+                    .build();
+        } catch (Exception e) {
+            logger.error("Прооизошла ошибка во время показа импортов: " + e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
