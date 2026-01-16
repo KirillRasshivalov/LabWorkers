ef77e6416c5d14761534117eefb357d900cf33b1
Kirill <kirill.rasshivalov@gmail.com>
2026-01-16 08:34:48 +0300

    Написал интерсепторы для кэша второго уровня.

---
+++ main/java/algo/demo/cache/CacheLogging.java
@@ -0,0 +1,10 @@
+package algo.demo.cache;
+
+import jakarta.interceptor.InterceptorBinding;
+import java.lang.annotation.*;
+
+@Inherited
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface CacheLogging {}
---
+++ main/java/algo/demo/cache/CacheLoggingInterceptor.java
@@ -0,0 +1,42 @@
+package algo.demo.cache;
+
+import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.Interceptor;
+import jakarta.interceptor.InvocationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+
+@Interceptor
+@CacheLogging
+public class CacheLoggingInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(CacheLoggingInterceptor.class);
+
+    private static boolean enabled = false;
+
+    @AroundInvoke
+    public Object logCacheStatistics(InvocationContext context) throws Exception {
+        Object result = context.proceed();
+
+        if (enabled) {
+            Method method = context.getMethod();
+            String className = method.getDeclaringClass().getSimpleName();
+            String methodName = method.getName();
+            logger.info("Статистика по кэшу для " + className + "." + methodName + "() =" );
+        }
+
+        return result;
+    }
+
+    public static void enable() {
+        enabled = true;
+        logger.info("Кэширование включено.");
+    }
+
+    public static void disable() {
+        enabled = false;
+        logger.info("Кэширование выключено.");
+    }
+}
